686a9235376f813b574e9b02c4982fda0842e267
Dennis Liu <dennis.liu2013@gmail.com>
2018-12-26 21:56:57 -0500

    - Refactored codes that hold test data in DocumentListController  - Added logic for deleting documents in document list  - ng-repeat in Document List is now track by index, in case there is an  duplicated element in the array

--- js/DocumentListController.js
+++ js/DocumentListController.js
@@ -2,16 +2,26 @@
 
 app.controller('DocumentListController', function($scope, $location, DocumentStoreService, ModalDataService) {
 	// Under construction
-	$scope.documents = [
-		{ id: "test", type: "table", description: "This is a test document"},
-		{ id: "eastWing", type: "table", description: "storing equipment serial # at east wing"},
-		{ id: "westWing", type: "table", description: "storing equipment serial # at east wing"},
-	];
+	$scope.documents = [];
 	
 	$scope.onView = function(doc) {
 		console.log(doc);
 		ModalDataService.setData("modalDocumentDisplay", {document: DocumentStoreService.getDocument(doc.id)});
 	};
 	
-	//$scope.documents = 
+	$scope.onDelete = function(idx) {
+		console.log(idx);
+		//DocumentStoreService.deleteDocument(id);
+	};
+	// Initialization
+	// Get a list of available documents
+	$scope.documents = DocumentStoreService.getDocumentList();
+	
+	var testData = [
+		{ id: "test", type: "table", description: "This is a test document"},
+		{ id: "eastWing", type: "table", description: "storing equipment serial # at east wing"},
+		{ id: "westWing", type: "table", description: "storing equipment serial # at west wing"},
+	];
+	console.log($scope.documents);
+	$scope.documents = testData;
 });
