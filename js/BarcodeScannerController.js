cf73666668255e74d78482113b196a739b9dcc82
Dennis Liu <dennis.liu2013@gmail.com>
2018-12-28 23:19:18 -0500

    - Slighly enlarged the height of scanner card  - Removed camera feed frame grab function since "capture" attribute of  the file input tag produce images that is far better in resolution and  quality

--- js/BarcodeScannerController.js
+++ js/BarcodeScannerController.js
@@ -5,12 +5,9 @@ app.controller('BarcodeScannerController', function($scope, $location) {
 	$scope.capturedImage = null;
 	$scope.cameraStatus = "initializing camera";
 	$scope.cameraDisabled = false;
+	$scope.cameras = [];
+	$scope.camera = null; // Current active camera
 	var video = document.getElementById("barcodeCaptureVideo");
-	var canvas = document.createElement("canvas");
-	var photo = document.getElementById("barcodeCapturedImage");
-	var streaming = false;
-	var width = 320;
-	var height = 0;
 	
 	var quaggaConfig = {
 		locate: true,
@@ -23,6 +20,14 @@ app.controller('BarcodeScannerController', function($scope, $location) {
     }
   };
 	
+	// On capture: just pretending capturing live feed
+	// while we are actually using input tag with capture attribute
+	// capture="camera" tells mobile browsers to activate cameras to acquire
+	// the image for upload
+	// Since this website is dedicated for usage on mobile devices so 
+	// It is safe to ignore desktop usages
+	// (It does not make sense for a relatively stationary laptop/desktop to scan barcodes)
+	$scope.onCapture = function() {$("#detectedBarcode").html("Processing");};
 	document.getElementById("barcodeCaptureUpload").onchange = function() {
 		console.log("image uploaded");
 		console.log(this);
@@ -31,30 +36,16 @@ app.controller('BarcodeScannerController', function($scope, $location) {
 		$scope.onAnalyze();
 	};
 	
-	$scope.onCapture = function() {
-		var context = canvas.getContext('2d');
-		if (width && height) {
-			$scope.cameraStatus = "captured";
-			canvas.width = width;
-			canvas.height = height;
-			context.drawImage(video, 0, 0, width, height);
-
-			var data = canvas.toDataURL('image/jpeg');
-			$scope.capturedImage = data;
-		}
-	};
-	
 	$scope.onAnalyze = function() {
-		console.log("Analyze!");
-		
 		$("#detectedBarcode").html("Processing");
 		// Call quagga lib to analyze the image
 		quaggaConfig.src = $scope.capturedImage;
-		Quagga.decodeSingle(quaggaConfig, function(result) {
-			if(result.codeResult) {
-				console.log("result", result.codeResult.code);
+		Quagga.decodeSingle(quaggaConfig, ffunction(result) {
+			if(result && result.codeResult) {
+				$("#detectedBarcode").html(result.codeResult.code);
+				$scope.$parent.$broadcast("BarcodeAnalyzed", result.codeResult.code);
 			} else {
-				console.log("not detected");
+				$("#detectedBarcode").html("Unable to distinguish barcode");
 			}
 		});
 		$scope.onClear();
@@ -64,17 +55,15 @@ app.controller('BarcodeScannerController', function($scope, $location) {
 		$scope.cameraStatus = "active";
 		$scope.capturedImage = null;
 	};
-	
-	$scope.cameras = [];
-	$scope.camera = null; // Current active camera
+	// Whenever user taps video tag, cycles the camera feed
 	$scope.onCycleCamera = function() {
 		$scope.cameraStatus = "initializing camera";
 		var idx = $scope.cameras.indexOf($scope.camera);
 		if(idx < 0) {/*No camera is found*/}
 		idx = (idx + 1) % $scope.cameras.length;
-		var deviceId = $scope.cameras[idx].deviceId;
+		$scope.camera = $scope.cameras[idx];
 		
-		navigator.mediaDevices.getUserMedia({ video: {deviceId: {exact: deviceId}}, audio: false}).
+		navigator.mediaDevices.getUserMedia({ video: {deviceId: {exact: $scope.camera.deviceId}}, audio: false}).
 			then(function(stream) {
 				video.srcObject = stream;
 				$scope.cameraStatus = "loading";
@@ -90,48 +79,10 @@ app.controller('BarcodeScannerController', function($scope, $location) {
 	navigator.mediaDevices.enumerateDevices().then(function(devices) {
 		$scope.cameras = devices.filter(v => v.kind == "videoinput");
 		$scope.camera = devices[0];
+		// Cycle the camera
+		// Since mobile devices might get front camera first
+		// Cycling once will switch to rear camera
+		// This can also help setup the stream to the video tag
 		$scope.onCycleCamera();
-	});
-	video.addEventListener('canplay', function(ev){
-		if (!streaming) {
-			height = video.videoHeight / (video.videoWidth/width);
-		
-			video.setAttribute('width', width);
-			video.setAttribute('height', height);
-			canvas.setAttribute('width', width);
-			canvas.setAttribute('height', height);
-			streaming = true;
-		}
-	}, false);
-
-	Quagga.onProcessed(function(result) {
-		if(result && result.codeResult) {
-			$("#detectedBarcode").html(result.codeResult.code);
-			$scope.$parent.$broadcast("BarcodeAnalyzed", result.codeResult.code);
-		} else {
-			$("#detectedBarcode").html("Unable to distinguish barcode");
-		}
-		/*
-		var drawingCtx = Quagga.canvas.ctx.overlay,
-				drawingCanvas = Quagga.canvas.dom.overlay;
-
-		if (result) {
-				if (result.boxes) {
-						drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-						result.boxes.filter(function (box) {
-								return box !== result.box;
-						}).forEach(function (box) {
-								Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-						});
-				}
-
-				if (result.box) {
-						Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
-				}
-
-				if (result.codeResult && result.codeResult.code) {
-						Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-				}
-		}*/
 	});
 });
