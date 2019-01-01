bd8b7cacc9a6fe8818c87830cd8cf796055abba5
Dennis Liu <dennis.liu2013@gmail.com>
2019-01-01 12:07:07 -0500

    - Added a npm package file and a simple NodeJS server to serve the  webpage so that it can be deployed on heroku

---
+++ barcorder.js
@@ -0,0 +1,11 @@
+// Heroku will define the port number in process.env.PORT
+const PORT = process.env.PORT || 3000;
+
+// Init
+var express = require("express");
+var app = express();
+app.use(express.static('.'));
+// Starts listening	
+app.listen(PORT, function () {
+	console.log('App listening on port ' + PORT)
+});
