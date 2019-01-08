78ff22e48feb80431a9cf19deb0e533a07f6ce08
Dennis Liu <dennis.liu2013@gmail.com>
2019-01-07 20:08:45 -0500

    - Reverted the decoded barcode list back to tap items to insert    instead of tapping on a dedicated button, which is very easy to    mis-tap in mobile phone and end up deleting the data  - Added custom data insertion field to Selected data list as well    in case user wants to insert unique custom data

--- frontend/js/SelectedDataController.js
+++ frontend/js/SelectedDataController.js
@@ -5,14 +5,18 @@ app.controller('SelectedDataController', function($scope, $location) {
 	
 	// Under construction
 	$scope.onBarcodeSelected = function(e, data) {
-		$scope.selectedData.push(data);
+		$scope.onInsert(data);
 	};
 	
 	$scope.onRemove = function(idx) {
 		$scope.selectedData.splice(idx, 1);
 	};
 	
-	$scope.onInsert = function() {
+	$scope.onInsert = function(data) {
+		$scope.selectedData.push(data);
+	};
+	
+	$scope.onAddToDocument = function() {
 		$scope.$parent.$broadcast("DocumentInsertRow", $scope.selectedData);
 	};
 	
